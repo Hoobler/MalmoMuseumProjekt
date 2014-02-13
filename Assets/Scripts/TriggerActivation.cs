bb29916b20caea104d941ab9af6d1ae30e0ffefa
Fredrik <f.wigarde@hotmail.com>
2014-02-13 14:56:32 +0100

    Lite ändringar

--- Assets/Scripts/TriggerActivation.cs
+++ Assets/Scripts/TriggerActivation.cs
@@ -3,12 +3,26 @@ using System.Collections;
 
 public class TriggerActivation : MonoBehaviour {
 
+	public bool test = false;
+
 	// Update is called once per frame
 	void Update () {
 	
 	}
 
-	void OnMouseDown(){
-		this.gameObject.SetActive (false);
+	void OnGUI(){
+		if (test) {
+			GUI.Label(new Rect(10, 10, 100, 100), "yey");		
+		}
 	}
+
+	void OnTriggerEnter(Collider other){
+
+		if (other.gameObject.tag == "Pickup") {	
+			other.gameObject.SetActive(false);
+			test = true;
+
+		}
+	}
+
 }
