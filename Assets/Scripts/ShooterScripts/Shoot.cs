f71563c6a4958a48dade15542d22716b7bff8be4
Joakim Persson <sir.hoobler@gmail.com>
2014-02-13 15:16:02 +0100

    FPS shootin
    Basic fps shooting

---
+++ Assets/Scripts/ShooterScripts/Shoot.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shoot : MonoBehaviour {
+	
+	public ChangeTargetColor script;
+
+	void Start () {
+	
+	}
+
+	void Update () {
+		RaycastHit hit;
+		Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+
+		if(Input.GetMouseButtonDown(0)){
+			if(Physics.Raycast(ray ,out hit, 100f)){
+
+				GameObject otherObj = hit.collider.gameObject;
+
+				if(otherObj.tag == "Target"){
+					script = (ChangeTargetColor) otherObj.GetComponent(typeof(ChangeTargetColor));
+					script.ChangeSize();
+				}
+			}
+		}
+ 	}
+}
