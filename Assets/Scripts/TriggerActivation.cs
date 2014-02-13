b4fa86ff629675f0b27c9a8a9f4d59682b9a2b55
Fredrik <f.wigarde@hotmail.com>
2014-02-13 12:18:17 +0100

    Ny scen
    Scen!

--- Assets/Scripts/TriggerActivation.cs
+++ Assets/Scripts/TriggerActivation.cs
@@ -3,13 +3,12 @@ using System.Collections;
 
 public class TriggerActivation : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	void OnMouseDown(){
+		this.gameObject.SetActive (false);
 	}
 }
