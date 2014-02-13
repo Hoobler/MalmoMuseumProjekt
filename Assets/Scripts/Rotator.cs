fd6ea2de78e09c36188b389849417deb2d4ccfd8
Fredrik <f.wigarde@hotmail.com>
2014-02-13 11:35:23 +0100

    Roterande Pickups

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (new Vector3 (0, 0, 15) * Time.deltaTime);
+	}
+}
