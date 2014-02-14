43514c339e8c9d84fbd141fcfae3206246f2b9be
Joakim Persson <sir.hoobler@gmail.com>
2014-02-14 14:43:09 +0100

    Escape för att få loss musen igen!

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -3,24 +3,25 @@ using System.Collections;
 
 public class PauseMenu : MonoBehaviour {
 
+	public GUIText guiText;
 	bool paused;
 
 	void Start () {
 		paused = false;
-		Screen.showCursor = false;
 		Debug.Log("PauseMenu Start");
+		guiText.text = "Null";
 	}
 
 	void Update () {
-
-		if(Input.GetKeyDown(KeyCode.Escape) && paused == false){
-			Screen.showCursor = true;
-			paused = true;
+		if(paused == false){
+			Screen.lockCursor = true;
 		}
 
-		if(Input.GetKeyDown(KeyCode.Escape) && paused == true){
-			Screen.showCursor = false;
-			paused = false;
+		if(Input.GetKeyDown(KeyCode.Escape)){
+			Screen.showCursor = true;
+			Screen.lockCursor = false;
+			paused = true;
+			guiText.text = "Paused";
 		}
 	}
 }
