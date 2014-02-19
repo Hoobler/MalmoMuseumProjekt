fd83175c1b2ee2743bb8816105c8956413c20311
Joakim Persson <sir.hoobler@gmail.com>
2014-02-19 14:45:25 +0100

    SpawnScript
    Spawning the right controller at a spawnpoint prefab

---
+++ Assets/Scripts/SpawnScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnScript : MonoBehaviour {
+	public Transform pcController;
+	public Transform androidController;
+	//public GameObject spawnObj;
+
+	void Start () {
+		//Later!
+		GameObject spawnObj = GameObject.FindGameObjectWithTag("SpawnPoint");
+		Vector3 spawnPos = spawnObj.transform.position;
+
+		Instantiate(pcController, spawnPos, Quaternion.identity);
+
+	#if UNITY_ANDROID
+		//Instantiate(androidController, spawnPos, Quaternion.identity);
+	#endif
+	}
+}
