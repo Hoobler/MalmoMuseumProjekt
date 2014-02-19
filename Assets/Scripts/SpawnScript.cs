74e7630ee1a24d1cf9aa8a66427c30b799168d6c
Joakim Persson <sir.hoobler@gmail.com>
2014-02-19 17:00:37 +0100

    SpawnScript
    Nu spawnas prefab in i DynamicObejects mappen, för att ha lite bättre
    ordning på allt skit.

--- Assets/Scripts/SpawnScript.cs
+++ Assets/Scripts/SpawnScript.cs
@@ -2,16 +2,19 @@
 using System.Collections;
 
 public class SpawnScript : MonoBehaviour {
-	public Transform pcController;
-	public Transform androidController;
-	//public GameObject spawnObj;
+	public GameObject pcController;
+	public GameObject androidController;
 
 	void Start () {
 		//Later!
 		GameObject spawnObj = GameObject.FindGameObjectWithTag("SpawnPoint");
+		GameObject parent = GameObject.FindGameObjectWithTag("DynamicObjects");
 		Vector3 spawnPos = spawnObj.transform.position;
+		GameObject temp = Instantiate(pcController, spawnPos, Quaternion.identity) as GameObject;
 
-		Instantiate(pcController, spawnPos, Quaternion.identity);
+		if(parent != null){
+		temp.transform.parent = parent.transform;
+		}
 
 	#if UNITY_ANDROID
 		//Instantiate(androidController, spawnPos, Quaternion.identity);
