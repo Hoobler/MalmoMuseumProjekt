9f4c66905f6805045bebb554c43425e7ee96f21d
Joakim Persson <sir.hoobler@gmail.com>
2014-02-20 16:48:15 +0100

    Musköt stuff
    Derp

--- Assets/Scripts/ShooterScripts/ChangeTargetColor.cs
+++ Assets/Scripts/ShooterScripts/ChangeTargetColor.cs
@@ -3,12 +3,30 @@ using System.Collections;
 
 public class ChangeTargetColor : MonoBehaviour {
 
-	// Use this for initialization
+	public float speed = 10f;
+	public Vector3 endPos;
+
+	private bool hit = false;
+	private bool reachedTarget = false;
+
 	void Start () {
-	
+
+	}
+
+	void Update(){
+		if(hit && !reachedTarget){
+			transform.position = Vector3.Slerp(transform.position, endPos, speed * Time.deltaTime);
+		}
+		//To stop it from updating and moving the target!
+		if(Vector3.Distance(transform.position, endPos) <= 0.1f){
+			Debug.Log("Stopp!");
+			reachedTarget = true;
+		}
 	}
 
 	public void ChangeSize(){
+		Debug.Log("ChangeSize");
+		hit = true;
 		transform.localScale = new Vector3(1f, 1f, 1f);
 	}
 }
