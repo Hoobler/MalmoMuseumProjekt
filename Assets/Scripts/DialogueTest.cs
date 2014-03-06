eb74657684be95b232bfcf013ae166dc062f271e
Bernhard Hedlund <orvarsmejl@hotmail.com>
2014-03-06 14:16:07 +0100

    Dialog, kollision
    Kycklingar kolliderar inte med spelaren,
    ändrade dialogstrukturen rätt mycket
    säkert mer men fuck it

--- Assets/Scripts/DialogueTest.cs
+++ Assets/Scripts/DialogueTest.cs
@@ -3,23 +3,22 @@ using System.Collections;
 
 [System.Serializable]
 public class Knapp{
-	public bool enabled = true;
 	public bool quitOnPress = false;
 	public string textOnButton = "Knapptext";
 	public string shownInfoWhenPressed = "Du tryckte på en knapp";
 	public Texture backgroundTexture;
 	public bool isQuestTrigger;
 	public string nameOfQuest;
+	[System.NonSerialized]
+	public GUITexture background;
+	[System.NonSerialized]
+	public GUIText text;
 }
 
 public class DialogueTest: MonoBehaviour {
 
 	public string introText = "Här ska det stå introduktionstext";
 	public Font font;
-	public Knapp button1;
-	public Knapp button2;
-	public Knapp button3;
-	public Knapp button4;
 
 	public Texture backgroundTexture;
 
@@ -28,16 +27,8 @@ public class DialogueTest: MonoBehaviour {
 	private QuestManager questManager;
 
 	GUIText mainGUIText;
-	GUIText button1GUIText;
-	GUIText button2GUIText;
-	GUIText button3GUIText;
-	GUIText button4GUIText;
 
 	GUITexture background;
-	GUITexture button1Back;
-	GUITexture button2Back;
-	GUITexture button3Back;
-	GUITexture button4Back;
 
 	GameObject dialogueObject;
 
@@ -45,22 +36,58 @@ public class DialogueTest: MonoBehaviour {
 
 	private bool conversationActive = false;
 
+	public Knapp[] buttons; 
+
 	void Start(){
 
 	}
 
 	void FormatMainText(){
-		string[] text = mainGUIText.text.Split(" "[0]);
+		string[] text = mainGUIText.text.Split(' ');
 		mainGUIText.text = "";
 
 		for (int i = 0; i < text.GetLength(0); i++) {
 
 			mainGUIText.text += text[i] + " ";
-			if(mainGUIText.GetScreenRect().width > 250)
+			if(mainGUIText.GetScreenRect().width > background.GetScreenRect().width * 0.8f)
 			{
 				mainGUIText.text = mainGUIText.text.Substring(0, mainGUIText.text.Length - text[i].Length - 1);
 				mainGUIText.text += "\n" + text[i] + " ";
 			}
+		}
+	}
+
+	void CreateNewButtons()
+	{
+		float offsetY = 0.045f;
+
+		for (int i = 0; i < buttons.Length; i++) {
+				GameObject buttonParent = new GameObject ("Button");
+				buttonParent.transform.parent = dialogueObject.transform;
+
+				GameObject backObject = new GameObject ("Background");
+				backObject.transform.parent = buttonParent.transform;
+				buttons [i].background = (GUITexture)backObject.AddComponent (typeof(GUITexture));
+				buttons [i].background.texture = buttons [i].backgroundTexture;
+			
+			buttons [i].background.transform.localScale = new Vector3 (0.20f, 0.07f, 0);
+				buttons [i].background.transform.position = new Vector3 (0.0f, 0f, 0.1f);
+		
+				GameObject textObject = new GameObject ("Text");
+				textObject.transform.parent = buttonParent.transform;
+				buttons [i].text = (GUIText)textObject.AddComponent (typeof(GUIText));
+				buttons [i].text.text = buttons [i].textOnButton;
+				buttons [i].text.transform.position = new Vector3 (0f, 0f, 0.11f);
+				buttons [i].text.transform.localScale = new Vector3 (0.1f, 0.05f, 0);
+				buttons [i].text.color = new Color (0, 0, 0);
+				buttons[i].text.alignment = TextAlignment.Center;
+				buttons[i].text.anchor = TextAnchor.MiddleCenter;
+				if (font)
+						buttons [i].text.font = font;
+				buttonParent.transform.localScale = new Vector3(1,1,1);
+				buttonParent.transform.position = new Vector3(0, -0.23f+offsetY*(buttons.Length-i - 1), 0.1f);
+
+
 		}
 	}
 
@@ -69,110 +96,34 @@ public class DialogueTest: MonoBehaviour {
 		dialogueObject = new GameObject ("Dialogue");
 		questManager = GameObject.Find ("Quest_Handler").GetComponent (typeof(QuestManager)) as QuestManager;
 
-		GameObject backObject = new GameObject ("DialogueBackground");
-		backObject.transform.parent = dialogueObject.transform;
+		GameObject backgroundHolder = new GameObject ("Background");
+		backgroundHolder.transform.parent = dialogueObject.transform;
+
+		GameObject backObject = new GameObject ("Background");
+		backObject.transform.parent = backgroundHolder.transform;
 		background = (GUITexture)backObject.AddComponent (typeof(GUITexture));
 		background.texture = backgroundTexture;
-		background.transform.position =  new Vector3 (0.2f, 0.5f, 0);
+		background.transform.position =  new Vector3 (0f, 0f, 0);
 		background.transform.localScale = new Vector3 (0.3f, 0.6f, 0);
 
-		GameObject mainGUITextObject = new GameObject ("DialogueMainText");
-		mainGUITextObject.transform.parent = dialogueObject.transform;
+		GameObject mainGUITextObject = new GameObject ("MainText");
+		mainGUITextObject.transform.parent = backgroundHolder.transform;
 		mainGUIText = (GUIText)mainGUITextObject.AddComponent (typeof(GUIText));
 		mainGUIText.text = introText;
-		mainGUIText.transform.position =  new Vector3 (0.08f, 0.75f, 0.10f);
+		mainGUIText.transform.position =  new Vector3 (-0.12f, 0.25f, 0.10f);
 		mainGUIText.transform.localScale = new Vector3 (0.1f, 0.05f, 0);
 		mainGUIText.color = new Color (0, 0, 0);
 		mainGUIText.lineSpacing = 1f;
 		if(font)
 			mainGUIText.font = font;
 
-		if (button1.enabled) {
-			GameObject button1BackObject = new GameObject ("DialogueButton1Background");
-			button1BackObject.transform.parent = dialogueObject.transform;
-			button1Back = (GUITexture)button1BackObject.AddComponent (typeof(GUITexture));
-			button1Back.texture = button1.backgroundTexture;
-			button1Back.transform.position = new Vector3 (0.12f, 0.4f, 0.1f);
-			button1Back.transform.localScale = new Vector3 (0.1f, 0.05f, 0);
-
-			GameObject button1TextObject = new GameObject ("DialogueButton1Text");
-			button1TextObject.transform.parent = dialogueObject.transform;
-			button1GUIText = (GUIText)button1TextObject.AddComponent (typeof(GUIText));
-			button1GUIText.text = button1.textOnButton;
-			button1GUIText.transform.position = new Vector3 (0.08f, 0.415f, 0.11f);
-			button1GUIText.transform.localScale = new Vector3 (0.1f, 0.05f, 0);
-			button1GUIText.color = new Color (0, 0, 0);
-			if(font)
-				button1GUIText.font = font;
-		}
-
-		if (button2.enabled) {
-			GameObject button2BackObject = new GameObject ("DialogueButton2Background");
-			button2BackObject.transform.parent = dialogueObject.transform;
-			button2Back = (GUITexture)button2BackObject.AddComponent (typeof(GUITexture));
-			button2Back.texture = button2.backgroundTexture;
-			button2Back.transform.position = new Vector3 (0.28f, 0.4f, 0.1f);
-			button2Back.transform.localScale = new Vector3 (0.1f, 0.05f, 0);
-			
-			GameObject button2TextObject = new GameObject ("DialogueButton2Text");
-			button2TextObject.transform.parent = dialogueObject.transform;
-			button2GUIText = (GUIText)button2TextObject.AddComponent (typeof(GUIText));
-			button2GUIText.text = button2.textOnButton;
-			button2GUIText.transform.position = new Vector3 (0.24f, 0.415f, 0.11f);
-			button2GUIText.transform.localScale = new Vector3 (0.1f, 0.05f, 0);
-			button2GUIText.color = new Color (0, 0, 0);
-			if(font)
-				button2GUIText.font = font;
-		}
-
-		if (button3.enabled) {
-			GameObject button3BackObject = new GameObject ("DialogueButton3Background");
-			button3BackObject.transform.parent = dialogueObject.transform;
-			button3Back = (GUITexture)button3BackObject.AddComponent (typeof(GUITexture));
-			button3Back.texture = button3.backgroundTexture;
-			button3Back.transform.position = new Vector3 (0.12f, 0.3f, 0.1f);
-			button3Back.transform.localScale = new Vector3 (0.1f, 0.05f, 0);
-
-			GameObject button3TextObject = new GameObject ("DialogueButton3Text");
-			button3TextObject.transform.parent = dialogueObject.transform;
-			button3GUIText = (GUIText)button3TextObject.AddComponent (typeof(GUIText));
-			button3GUIText.text = button3.textOnButton;
-			button3GUIText.transform.position = new Vector3 (0.08f, 0.315f, 0.11f);
-			button3GUIText.transform.localScale = new Vector3 (0.1f, 0.05f, 0);
-			button3GUIText.color = new Color (0, 0, 0);
-			if(font)
-				button3GUIText.font = font;
-		}
-
-		if (button4.enabled) {
-			GameObject button4BackObject = new GameObject ("DialogueButton4Background");
-			button4BackObject.transform.parent = dialogueObject.transform;
-			button4Back = (GUITexture)button4BackObject.AddComponent (typeof(GUITexture));
-			button4Back.texture = button4.backgroundTexture;
-			button4Back.transform.position = new Vector3 (0.28f, 0.3f, 0.1f);
-			button4Back.transform.localScale = new Vector3 (0.1f, 0.05f, 0);
-
-			GameObject button4TextObject = new GameObject ("DialogueButton4Text");
-			button4TextObject.transform.parent = dialogueObject.transform;
-			button4GUIText = (GUIText)button4TextObject.AddComponent (typeof(GUIText));
-			button4GUIText.text = button4.textOnButton;
-			button4GUIText.transform.position = new Vector3 (0.24f, 0.315f, 0.11f);
-			button4GUIText.transform.localScale = new Vector3 (0.1f, 0.05f, 0);
-			button4GUIText.color = new Color (0, 0, 0);
-			if(font)
-				button4GUIText.font = font;
-		}
+		CreateNewButtons ();
+		dialogueObject.transform.position = new Vector3 (0.2f, 0.5f, 0);
 
 		FormatMainText ();
-		//SendMessage ("QuestTrigger", 5);
-
 
 	}
 
-//	void QuestTrigger(int number){
-//		buttonQuitGUIText.text = "" + number;
-//		Destroy (buttonQuitBackObject);
-//		}
 
 	void KillConversation(){
 		Destroy (dialogueObject);
@@ -182,49 +133,28 @@ public class DialogueTest: MonoBehaviour {
 	void Update(){
 		if (conversationActive) {
 			if (Input.GetMouseButtonDown (0)) {
-				if (button1Back != null)
-					if (button1Back.GetScreenRect ().Contains (Input.mousePosition)) {
-						mainGUIText.text = button1.shownInfoWhenPressed;
-						
-						if (questManager && button1.isQuestTrigger)
-							questManager.ActivateQuest(button1.nameOfQuest);
+				for(int i = 0; i < buttons.Length; i++)
+				{
+					if (buttons[i].background.GetScreenRect ().Contains (Input.mousePosition)) {
 
-						if(button1.quitOnPress)
-							KillConversation();
-					}
-								
-				if (button2Back != null)
-					if (button2Back.GetScreenRect ().Contains (Input.mousePosition)) {
-						mainGUIText.text = button2.shownInfoWhenPressed;
-						if (questManager && button2.isQuestTrigger)
-							questManager.ActivateQuest(button2.nameOfQuest);
-						if(button2.quitOnPress)
-							KillConversation();
-					}
-								
-				if (button3Back != null)
-					if (button3Back.GetScreenRect ().Contains (Input.mousePosition)) {
-						mainGUIText.text = button3.shownInfoWhenPressed;
-						if (questManager && button3.isQuestTrigger)
-							questManager.ActivateQuest(button3.nameOfQuest);
-						if(button3.quitOnPress)
-							KillConversation();
-					}
-								
-				if (button4Back != null)
-					if (button4Back.GetScreenRect ().Contains (Input.mousePosition)) {
-						mainGUIText.text = button4.shownInfoWhenPressed;
-						if (questManager && button4.isQuestTrigger)
-							questManager.ActivateQuest(button4.nameOfQuest);
-						if(button4.quitOnPress)
-							KillConversation();
-					}
-				FormatMainText();
+						if (questManager && buttons[i].isQuestTrigger)
+							questManager.ActivateQuest(buttons[i].nameOfQuest);
 
+						if(buttons[i].quitOnPress)
+						{
+							KillConversation();
+							break;
+						}
+
+						mainGUIText.text = buttons[i].shownInfoWhenPressed;
+						FormatMainText();
+						break;
+					}
+				}
 			}
 			if(playerTransform && Vector3.Distance(playerTransform.position, this.transform.position) > abortConversationDistance)
 				KillConversation();
-				}
+		}
 	}
 	void OnMouseDown(){
 		playerTransform = GameObject.FindGameObjectWithTag ("Player").transform;
@@ -234,4 +164,5 @@ public class DialogueTest: MonoBehaviour {
 				conversationActive = true;
 			}		
 	}
+
 }
---
+++ Assets/buttonScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class buttonScript : MonoBehaviour {
+
+	public GameObject dialogueObject;
+	public DialogueTest dialogue;
+
+	public string TextShown;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseDown(){
+	}
+}
