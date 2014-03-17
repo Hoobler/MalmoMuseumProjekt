ad56b729cca7c50bdd66a036efec355268d28a11
Joakim Persson <sir.hoobler@gmail.com>
2014-03-17 15:14:51 +0100

    InputScripts
    So much scripts

---
+++ Assets/Scripts/InputScripts/TouchCamera.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TouchCamera : TouchInput {
+
+	public float RotationSpeed;
+	public bool InvertPitch;
+	public Transform player;
+
+	private int _inverted;
+	private float _pitch;
+	private float _yaw;
+	private Vector3 _oRotation;
+	
+	void Start () {
+		_oRotation = Camera.main.transform.eulerAngles;
+		_pitch = _oRotation.x;
+		_yaw = _oRotation.y;
+		if(InvertPitch){
+			_inverted = -1;
+		}
+		else{
+			_inverted = 1;
+		}
+	}
+
+	void Update () {
+	}
+
+	void OnTouchBegan(){
+		touchToCheck = TouchInput.currTouch;
+	}
+
+	public void OnTouchMoved(){
+		_pitch -= Input.GetTouch(touchToCheck).deltaPosition.y * RotationSpeed * _inverted * Time.deltaTime;
+		_yaw += Input.GetTouch(touchToCheck).deltaPosition.x * RotationSpeed * _inverted * Time.deltaTime;
+
+		//Limit so you won't break your back!
+		_pitch = Mathf.Clamp(_pitch, -80, 80);
+
+		//Add the rotation to the camera!
+		Camera.main.transform.eulerAngles = new Vector3(_pitch, _yaw, 0.0f);
+	}
+
+	void OnTouchEnded(){
+		if(TouchInput.currTouch == touchToCheck || Input.touches.Length <= 0)
+			touchToCheck = 64;
+	}
+}
---
+++ Assets/Scripts/InputScripts/TouchInput.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TouchInput : MonoBehaviour {
+
+	public static int currTouch;
+	[HideInInspector]
+	public int touchToCheck;
+
+	void Start () {
+		currTouch = 0;
+		touchToCheck = 0;
+	}
+
+	void Update () {
+
+		//Lägg till så den kan kolla om du träffar en guiTexture senare.
+		//Så går det att använda för allt!
+		if(Input.touches.Length == 0){
+			DoNothing();
+		}
+		else{
+			for(int i = 0; i < Input.touchCount; i++){
+				currTouch = i;
+				if(Input.GetTouch(i).phase == TouchPhase.Began){
+					OnTouchBegan();
+				}
+				if(Input.GetTouch(i).phase == TouchPhase.Moved){
+					OnTouchMoved();
+				}
+				if(Input.GetTouch(i).phase == TouchPhase.Ended){
+					OnTouchEnded();
+				}
+				if(Input.GetTouch(i).phase == TouchPhase.Stationary){
+					OnTouchStationary();
+				}
+			}
+		}
+	}
+
+	public virtual void DoNothing(){}
+	public virtual void OnTouchBegan(){}
+	public virtual void OnTouchMoved(){}
+	public virtual void OnTouchEnded(){}
+	public virtual void OnTouchStationary(){}
+}
