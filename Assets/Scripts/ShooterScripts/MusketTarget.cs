0d988a31653d0bcec30ea9552784c4e1ecab3573
Joakim Persson <sir.hoobler@gmail.com>
2014-03-20 16:15:36 +0100

    Target rör sig mellan två punkter nu.
    Sätt inte +=  på din speed som en idiot bara!

--- Assets/Scripts/ShooterScripts/MusketTarget.cs
+++ Assets/Scripts/ShooterScripts/MusketTarget.cs
@@ -3,49 +3,86 @@ using System.Collections;
 
 public class MusketTarget : MonoBehaviour {
 	
-	public float speed = 2f;
-	//public Transform endPos;
+	public float Speed = 2f;
+	public Transform[] triggers;
 	public int targetID;
 	
-	private bool hit = false;
-	private bool questTrigger = false;
-	private bool reachedTarget = false;
+	private bool _hit = false;
+	private bool _questTrigger = false;
+	private bool _reachedTarget = false;
+	private bool _stopped;
 
 	void Start () {
 		EventManager.OnHit += EventRespons;
 		EventManager.OnQuest += EventReset;
+		StartCoroutine(MoveTarget(triggers[0].position, triggers[1].position));
 	}
 	
 	void Update(){
 //		if(hit && !reachedTarget){
 //			StartCoroutine(MoveTarget(transform, transform.position, endPos.position, speed));
 //		}
-		//To stop it from updating and moving the target!
-//		if(Vector3.Distance(transform.position, endPos.position) <= 0.1f && !reachedTarget){
-//			Debug.Log("Stopp!");
-//			reachedTarget = true;
-//		}
 
-		if(hit && !questTrigger){
+		if(_hit && !_questTrigger){
 			EventManager.TriggerOnQuest(MiniGamesEnum.Musköt , new QuestEventArgs(QuestTypeEnum.OnGoing, "stuff"));
 		}
-		if(questTrigger && reachedTarget){
+		if(_questTrigger && _reachedTarget){
 			EventManager.TriggerOnQuest(MiniGamesEnum.Musköt , new QuestEventArgs(QuestTypeEnum.GoalReached, "stuff"));
 		}
 	}
 
 	void ResetTarget(){}
 
-	IEnumerator MoveTarget(Transform mTransform, Vector3 startPos, Vector3 endPos, float time){
-		float i = 0f;
-		float rate = 1.0f/time;
-		while(i < 1.0f){
-			i += Time.deltaTime * rate;
-			mTransform.position = Vector3.Lerp(startPos, endPos, i);
-			yield return null;
+	//Old
+//	IEnumerator MoveTarget(Transform mTransform, Vector3 startPos, Vector3 endPos, float time){
+//		float i = 0f;
+//		float rate = 1.0f/time;
+//		while(i < 1.0f){
+//			if(!
+//			i += Time.deltaTime * rate;
+//			mTransform.position = Vector3.Lerp(startPos, endPos, i);
+//			yield return null;
+//		}
+//	}
+
+	//New ska stoppa target från att röra sig när questen inte är aktiv
+	//Resten av tiden sak den åka mellan leftPos och rightPos
+	IEnumerator MoveTarget(Vector3 leftPos, Vector3 rightPos){
+		float distLeft, distRight;
+		float rate = 0;
+		bool goLeft = false;
+		bool goRight = true;
+		while(true){
+			if(_stopped){
+				yield return new WaitForSeconds(0.5f);
+			} else {
+				distLeft = Vector3.Distance(this.transform.position, leftPos);
+				distRight = Vector3.Distance(this.transform.position, rightPos);
+				rate = Time.deltaTime * Speed;
+
+				if(distRight <= 0.01f){
+					goLeft = true;
+					goRight = false;
+				}
+
+				if(distLeft <= 0.01f){
+					goLeft = false;
+					goRight = true;
+				}
+
+				if(goRight){
+					this.transform.position = Vector3.MoveTowards(transform.position, rightPos, rate);
+					yield return null;
+				}
+				if(goLeft){
+					this.transform.position = Vector3.MoveTowards(transform.position, leftPos, rate);
+					yield return null;
+				}
+			}
 		}
 	}
 
+	//Tabort senare antaligen! Yes Tabort!
 	void EventReset(MiniGamesEnum miniEnum , QuestEventArgs evArgs){
 		if(miniEnum == MiniGamesEnum.Musköt){
 			if(evArgs.QuestType == QuestTypeEnum.Reset){
@@ -57,7 +94,7 @@ public class MusketTarget : MonoBehaviour {
 	void EventRespons(int id){
 		if(targetID == id){
 			Debug.Log("Target hit! " + id);
-			hit = true;
+			_hit = true;
 		}
 	}
 }
