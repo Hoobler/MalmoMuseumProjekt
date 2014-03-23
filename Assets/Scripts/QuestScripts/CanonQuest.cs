57f6c5938b3353e2290bf19eda4a536711c33b27
Fredrik <f.wigarde@hotmail.com>
2014-03-23 19:55:55 +0100

    Canon quest buggat

--- Assets/Scripts/QuestScripts/CanonQuest.cs
+++ Assets/Scripts/QuestScripts/CanonQuest.cs
@@ -10,6 +10,8 @@ public class CanonQuest : QuestBase {
 	GameObject canonCamera;
 	GameObject canon;
 	GameObject ship;
+
+	private bool questActive = false;
 	
 
 	public override void TriggerStart ()
@@ -25,6 +27,8 @@ public class CanonQuest : QuestBase {
 		canonCamera.camera.enabled = true;
 
 		canonballs_shot = 0;
+
+		questActive = true;
 
 		InitScreenComponents ();
 
@@ -43,7 +47,13 @@ public class CanonQuest : QuestBase {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		float speed = 20f;
+		if (questActive) {
+			if (Input.GetKeyDown (KeyCode.A))
+				canon.transform.Rotate (Vector3.up * speed * Time.deltaTime);
+			if (Input.GetKeyDown (KeyCode.D))
+				canon.transform.Rotate (-Vector3.up * speed * Time.deltaTime);
+		}
 	}
 
 	void InitScreenComponents(){
