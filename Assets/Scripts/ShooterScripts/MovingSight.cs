94b9942909be9d815cecbe35d5b16cad6193c1b5
Joakim Persson <sir.hoobler@gmail.com>
2014-03-31 16:03:15 +0200

    Siktet gungar nu!
    Should work!

--- Assets/Scripts/ShooterScripts/MovingSight.cs
+++ Assets/Scripts/ShooterScripts/MovingSight.cs
@@ -39,26 +39,32 @@ public class MovingSight : MonoBehaviour {
 		while(true){
 			if(!_active){ yield return new WaitForSeconds(2.0f); }
 			else{
-				if(index > RotationMatrix.Length -1){
-					index = 0;
-				}
-				if(newTime){
-					timeLeft = RotationMatrix[index].time;
-					newTime = false;
-				}
-				_rotationVector = new Vector3(RotationMatrix[index].MinRangeX,
-				                              RotationMatrix[index].MaxRangeX,
-				                              0/*Camera.main.transform.rotation.z*/);
+//				if(index > RotationMatrix.Length -1){
+//					index = 0;
+//				}
+//				if(newTime){
+//					timeLeft = RotationMatrix[index].time;
+//					newTime = false;
+//				}
+//				_rotationVector = new Vector3(RotationMatrix[index].MinRangeX,
+//				                              RotationMatrix[index].MaxRangeX,
+//				                              0/*Camera.main.transform.rotation.z*/);
 
-				Debug.Log(timeLeft);
-				if(timeLeft >=0){;
-					Camera.main.transform.rotation = Quaternion.Slerp(Camera.main.transform.rotation,(Camera.main.transform.rotation * Quaternion.Euler(_rotationVector)), Time.deltaTime);
-					timeLeft -= Time.deltaTime;
-				}
-				if(timeLeft <= 0){
-					index++;
-					newTime = true;
-				}
+				float t = Time.time;
+				float y = Mathf.Sin(t) * 20.0f;
+				float x = Mathf.Cos(t* 3) * 10.0f;
+				_rotationVector = new Vector3(x, y, 0.0f);
+
+
+//				Debug.Log(timeLeft);
+//				if(timeLeft >=0){;
+				Camera.main.transform.rotation = Quaternion.Slerp(Camera.main.transform.rotation,(Camera.main.transform.rotation * Quaternion.Euler(_rotationVector)), Time.deltaTime * 0.2f);
+//					timeLeft -= Time.deltaTime;
+//				}
+//				if(timeLeft <= 0){
+//					index++;
+//					newTime = true;
+//				}
 				yield return null;
 			}
 		}
