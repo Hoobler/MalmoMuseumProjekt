508975ab6ff25e7a4c60d70aef8800be16b307ce
Joakim Persson <sir.hoobler@gmail.com>
2014-04-01 16:04:45 +0200

    Commit again!

--- Assets/Scripts/ShooterScripts/MovingSight.cs
+++ Assets/Scripts/ShooterScripts/MovingSight.cs
@@ -23,13 +23,8 @@ public class MovingSight : MonoBehaviour {
 
 	void Start () {
 		EventManager.OnQuest += EventRespons;
-		//_active = true;
 		StartCoroutine(SightSway());
 	}
-
-	void Update () {
-		//Camera.main.transform.rotation = Quaternion.Slerp(Camera.main.transform.rotation, Quaternion.Euler(_rotationVector), Time.deltaTime);
-	}	
 
 	IEnumerator SightSway(){
 		int index = 0;
@@ -38,37 +33,19 @@ public class MovingSight : MonoBehaviour {
 		while(true){
 			if(!_active){ yield return new WaitForSeconds(2.0f); }
 			else{
-//				if(index > RotationMatrix.Length -1){
-//					index = 0;
-//				}
-//				if(newTime){
-//					timeLeft = RotationMatrix[index].time;
-//					newTime = false;
-//				}
-//				_rotationVector = new Vector3(RotationMatrix[index].MinRangeX,
-//				                              RotationMatrix[index].MaxRangeX,
-//				                              0/*Camera.main.transform.rotation.z*/);
-
 				float t = Time.time;
 				float y = Mathf.Sin(t) * 20.0f;
 				float x = Mathf.Cos(t* 3) * 10.0f;
 				_rotationVector = new Vector3(x, y, 0.0f);
 
-
-//				Debug.Log(timeLeft);
-//				if(timeLeft >=0){;
 				Camera.main.transform.rotation = Quaternion.Slerp(Camera.main.transform.rotation,(Camera.main.transform.rotation * Quaternion.Euler(_rotationVector)), Time.deltaTime * 0.2f);
-//					timeLeft -= Time.deltaTime;
-//				}
-//				if(timeLeft <= 0){
-//					index++;
-//					newTime = true;
-//				}
+
 				yield return null;
 			}
 		}
 	}
 
+	//lol!
 	void EventRespons(MiniGamesEnum miniEnum , QuestEventArgs evArgs){
 		if(miniEnum == MiniGamesEnum.Musköt){
 			if(evArgs.QuestType == QuestTypeEnum.Started){
