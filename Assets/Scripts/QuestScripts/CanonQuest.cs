bd3af89cf4773f14356879010f4a797121b410d7
Fredrik <f.wigarde@hotmail.com>
2014-04-01 12:34:47 +0200

    Kanon skit

--- Assets/Scripts/QuestScripts/CanonQuest.cs
+++ Assets/Scripts/QuestScripts/CanonQuest.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class CanonQuest : QuestBase {
@@ -6,7 +6,10 @@ public class CanonQuest : QuestBase {
 	private const int TOTAL_CANONBALLS = 3;
 	private int canonballs_shot;
 
-	private const float ROTATION_SPEED = 20;
+	private const float ROTATION_SPEED = 10;
+	private Vector3 start_side_rotation;
+	private Vector3 start_up_rotation;
+
 
 	GameObject mainCamera;
 	GameObject canonCamera;
@@ -29,25 +32,28 @@ public class CanonQuest : QuestBase {
 		canonPipe 	= GameObject.FindGameObjectWithTag ("KanonPipa");
 		ship 		= GameObject.Find ("Ship");
 		player 		= GameObject.FindGameObjectWithTag ("Player");
+		
 
-		mainCamera.camera.enabled = false;
-		canonCamera.camera.enabled = true;
+		mainCamera.SetActive(false);
+		canonCamera.SetActive (true);
 
 		canonballs_shot = 0;
 
 		questActive = true;
+<<<<<<< HEAD
+=======
 
 		//player.SetActive (false);
 
 		//InitScreenComponents ();
 
+>>>>>>> aaac566043485b97d5da25ce05a92a6b02724c4e
 	}
 
 	public override void TriggerFinish ()
 	{
-		mainCamera.camera.enabled = true;
-		canonCamera.camera.enabled = false;
-		//player.SetActive (true);
+		mainCamera.SetActive (true);
+		canonCamera.SetActive (false);
 	}
 
 	// Use this for initialization
@@ -58,14 +64,22 @@ public class CanonQuest : QuestBase {
 	// Update is called once per frame
 	void Update () {
 		if (questActive) {
-			if (Input.GetKey (KeyCode.A))
-				canon.transform.Rotate(-Vector3.up * ROTATION_SPEED * Time.deltaTime); 
-			if (Input.GetKey (KeyCode.D))
+			if (Input.GetKey (KeyCode.A) && start_side_rotation.y > -10f) {
+				canon.transform.Rotate(-Vector3.up * ROTATION_SPEED * Time.deltaTime);
+				start_side_rotation += -Vector3.up * ROTATION_SPEED * Time.deltaTime;
+			}
+			if (Input.GetKey (KeyCode.D) && start_side_rotation.y < 10f){
 				canon.transform.Rotate(Vector3.up * ROTATION_SPEED * Time.deltaTime);
-			if (Input.GetKey (KeyCode.W))
+				start_side_rotation += Vector3.up * ROTATION_SPEED * Time.deltaTime;
+			}
+			if (Input.GetKey (KeyCode.W) && start_up_rotation.x > -15f){
 				canonPipe.transform.Rotate(Vector3.left * ROTATION_SPEED * Time.deltaTime); 
-			if (Input.GetKey (KeyCode.S))
+				start_up_rotation += Vector3.left * ROTATION_SPEED * Time.deltaTime;
+			}
+			if (Input.GetKey (KeyCode.S) && start_up_rotation.x < 0f){
 				canonPipe.transform.Rotate(Vector3.right * ROTATION_SPEED * Time.deltaTime);
+				start_up_rotation += Vector3.right * ROTATION_SPEED * Time.deltaTime;
+			}
 
 		}
 	}
