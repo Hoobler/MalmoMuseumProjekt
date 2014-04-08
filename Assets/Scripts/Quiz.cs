66db60512558ff77a1c8ae9fae1ac876f060255e
Bernhard Hedlund <orvarsmejl@hotmail.com>
2014-04-08 13:28:30 +0200

    queeez

--- Assets/Scripts/Quiz.cs
+++ Assets/Scripts/Quiz.cs
@@ -6,59 +6,119 @@ public class Question{
 	public string question;
 	public string[] answers;
 	public int correctAnswer;
-	//public Texture backgroundTexture;
-	//[System.NonSerialized]
-	//public GUITexture background;
-	//[System.NonSerialized]
-	//public GUIText text;
 }
 
 public class Quiz : MonoBehaviour {
 
 	public Question[] questions;
-
+	public bool randomizeQuestions;
+	Rect backgroundBounds;
 	GUIText questionText;
 	GameObject[] buttons;
-	int currentQuestion = 0;
+	int currentQuestion;
 	GameObject quizParent;
-
-	// Use this for initialization
-	void Start () {
-	
+	bool active = false;
+	int points;
+//
+//	// Use this for initialization
+//	void Start () {
+//	
+//	}
+//
+//
+	void EndScreen()
+	{
+		if (points == 0)
+			questionText.text = "Du klarade inte en enda fråga. =(";
+		else if (points == questions.Length)
+			questionText.text = "Grattis, du klarade alla frågor!";
+		else
+			questionText.text = "Grattis! Du klarade " + points + " av " + questions.Length + " frågor!";
+		FormatMainText ();
+		for (int i = 0; i < buttons.Length; i++) {
+			SetButtonEnabled(buttons[i], false);
+		}
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	void Update() {
+		if (active) {
+			if(Input.GetMouseButtonDown(0))
+			{
+				if(currentQuestion < questions.Length)
+				{
+					for(int i = 0; i < questions[currentQuestion].answers.Length; i++)
+					{
+						if(((GUITexture)buttons[i].GetComponentInChildren(typeof(GUITexture))).GetScreenRect().Contains(Input.mousePosition))
+						{
+							if(i == questions[currentQuestion].correctAnswer)
+								points++;
+							NextQuestion ();
+							break;
+						}
+					}
+				}
+				else
+				{
+					TriggerFinish();
+				}
+			}
+		}
 	}
 
-	void TriggerStart()
+	void FormatMainText(){
+		string[] text = questionText.text.Split(' ');
+		questionText.text = "";
+		
+		for (int i = 0; i < text.GetLength(0); i++) {
+			
+			questionText.text += text[i] + " ";
+			if(questionText.GetScreenRect().width > backgroundBounds.width * 0.8f)
+			{
+				questionText.text = questionText.text.Substring(0, questionText.text.Length - text[i].Length - 1);
+				questionText.text += "\n" + text[i] + " ";
+			}
+		}
+	}
+
+	public void TriggerStart()
 	{
 		quizParent = new GameObject ("QuizParent");
+		GameObject background = (GameObject)Instantiate (Resources.Load ("Quizmain"));
+		background.transform.parent = quizParent.transform;
+		backgroundBounds = ((GUITexture)background.GetComponentInChildren (typeof(GUITexture))).GetScreenRect ();
+		questionText = (GUIText)background.GetComponentInChildren (typeof(GUIText));
+		currentQuestion = 0;
+		points = 0;
 		buttons = new GameObject[4];
 		for (int i = 0; i < 4; i++) {
 			buttons [i] = (GameObject)Instantiate (Resources.Load ("Button"));
 			buttons[i].transform.parent = quizParent.transform;
 			SetButtonText(buttons[i], questions[0].answers[i]);
-			buttons[i].transform.position = new Vector3(0.25f*i%2, 0.25f*(i/2), 0); 
+			buttons[i].transform.position = new Vector3(0.25f + 0.25f*(i%2), 0.25f+0.25f*(i/2), 0); 
 		}
 
 		SetValues ();
 	}
 
-	void TriggerFinish()
+	public void TriggerFinish()
 	{
+		active = false;
 		Destroy (quizParent);
 	}
 
 	void NextQuestion()
 	{
 		currentQuestion++;
-		SetValues ();
+		if (currentQuestion >= questions.Length)
+			EndScreen ();
+		else
+			SetValues ();
 	}
 
 	void SetValues()
 	{
+		questionText.text = questions [currentQuestion].question;
+		FormatMainText ();
 		for (int i = 0; i < questions[currentQuestion].answers.Length; i++) { 
 			SetButtonText(buttons [i], questions [currentQuestion].answers [i]);
 			SetButtonEnabled(buttons[i], true);
@@ -78,7 +138,14 @@ public class Quiz : MonoBehaviour {
 
 	void SetButtonText(GameObject button, string text)
 	{
-		GUIText guiText = (GUIText)button.GetComponentInChildren (typeof(GUIText));
-		guiText.text = text;
+		((GUIText)button.GetComponentInChildren (typeof(GUIText))).text = text;
+	}
+
+	void OnMouseDown()
+	{
+		if (!active) {
+			TriggerStart ();
+			active = true;
+		}
 	}
 }
