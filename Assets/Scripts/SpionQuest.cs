80b7303d49a087513e1ac567cd24c0af77f96596
Bernhard Hedlund <orvarsmejl@hotmail.com>
2014-04-21 11:00:55 +0200

    Spion basic
    Bara bas

---
+++ Assets/Scripts/SpionQuest.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpionQuest : QuestBase {
+
+
+	GameObject spionA;
+	GameObject spionB;
+	GameObject spionC;
+
+	bool questActive;
+
+
+	// Use this for initialization
+	void Start () {
+		spionA = GameObject.Find ("SpionA");
+		spionB = GameObject.Find ("SpionB");
+		spionC = GameObject.Find ("SpionC");
+		questActive = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public override void TriggerStart ()
+	{
+		questActive = false;
+
+	}
+
+	public override void TriggerFinish ()
+	{
+
+
+	}
+
+	void SpyCheck(int chosen)
+	{
+
+	}
+
+	public void SpyTrigger(GameObject other)
+	{
+		if (other.name == "SpionA")
+			SpyCheck(0);
+		else if (other.name == "SpionB")
+			SpyCheck(1);
+		else if(other.name == "SpionC")
+			SpyCheck(2);
+	}
+}
---
+++ Assets/Scripts/SpionVictimScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpionVictimScript : MonoBehaviour {
+
+	SpionQuest sQuest;
+
+	// Use this for initialization
+	void Start () {
+		sQuest = (SpionQuest)GameObject.Find ("QuestGiverSpion").GetComponent (typeof(SpionQuest));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseDown()
+	{
+		sQuest.SpyTrigger (gameObject);
+	}
+}
