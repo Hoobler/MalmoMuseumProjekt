1b270b729060186f0d120390815cdf9121f289a0
Fredrik <f.wigarde@hotmail.com>
2014-04-28 12:54:06 +0200

    Script till kanonen

--- Assets/Scripts/QuestScripts/Quests/Canon/ShipScript.cs
+++ Assets/Scripts/QuestScripts/Quests/Canon/ShipScript.cs
@@ -9,6 +9,7 @@ public class ShipScript : MonoBehaviour {
 	Vector3 speed;
 	public bool hit = false;
 	private const int ROTATION_SPEED = -5;
+	Quaternion rotation;
 
 	// Use this for initialization
 	void Start () {
@@ -23,6 +24,7 @@ public class ShipScript : MonoBehaviour {
 
 		distance.Normalize();
 
+		rotation = gameObject.transform.rotation;
 	}
 	
 	// Update is called once per frame
@@ -30,6 +32,13 @@ public class ShipScript : MonoBehaviour {
 		gameObject.transform.position += new Vector3 (distance.x * speed.x, speed.y, distance.z * speed.z);
 		if (hit)
 			gameObject.transform.Rotate (Vector3.forward * ROTATION_SPEED * Time.deltaTime);
+	}
+
+	//Resets
+	public void Reset(){
+		gameObject.transform.position = startPoint.transform.position;
+		gameObject.transform.rotation = rotation;
+
 	}
 
 	public Vector3 Speed{
