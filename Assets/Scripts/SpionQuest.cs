f0c859fd68e29e1ff96bb3c9f953b15d54d20f5a
Bernhard Hedlund <orvarsmejl@hotmail.com>
2014-04-28 13:32:41 +0200

    Spioneri
    Akta er för Mads

--- Assets/Scripts/SpionQuest.cs
+++ Assets/Scripts/SpionQuest.cs
@@ -3,8 +3,13 @@ using System.Collections;
 
 public class SpionQuest : QuestBase {
 
+	public Texture background;
+	public Texture[] spionTexturer;
+	public string[] namn;
 	bool questActive;
 	GameObject dialogueWindow;
+
+	Rect[] choiceBounds;
 
 	// Use this for initialization
 	void Start () {
@@ -13,18 +18,56 @@ public class SpionQuest : QuestBase {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if(questActive){
+			if(Input.GetMouseButtonUp(0))
+			{
+				for(int i = 0; i < 3; i++)
+				{
+					if(choiceBounds[i].Contains(Input.mousePosition))
+					{
+						TriggerFinish();
+					}
+				}
+			}
+		}
 	}
 
 	public override void TriggerStart ()
 	{
+		choiceBounds = new Rect[3];
 		questActive = true;
-		//dialogueWindow = (GameObject)Instantiate("
+		dialogueWindow = new GameObject ("SpionFonster");
+		dialogueWindow.transform.localScale = Vector3.zero;
+		dialogueWindow.transform.position = new Vector3 (0, 0, -1);
+		GUITexture backTexture = dialogueWindow.AddComponent<GUITexture> ();
+		backTexture.texture = background;
+		backTexture.pixelInset = new Rect (Screen.width * 0.3f, Screen.height * 0.3f, Screen.width * 0.4f, Screen.height * 0.4f);
+
+		for(int i = 0; i < 3; i++)
+		{
+			GameObject val = new GameObject("Valruta"+i);
+			val.transform.localScale = Vector3.zero;
+			val.transform.parent = dialogueWindow.transform;
+			GUITexture guiTexture = val.AddComponent<GUITexture>();
+			guiTexture.texture = spionTexturer[i];
+			choiceBounds[i] = new Rect(backTexture.GetScreenRect().x + backTexture.GetScreenRect().width*0.05f + backTexture.GetScreenRect ().width*0.3f*i,
+			                           backTexture.GetScreenRect().y + backTexture.GetScreenRect ().height*0.15f,
+			                           backTexture.GetScreenRect ().width*0.29f,
+			                           backTexture.GetScreenRect().height*0.8f);
+			guiTexture.pixelInset = choiceBounds[i];
+			GUIText guiText = val.AddComponent<GUIText>();
+			guiText.text = namn[i];
+			guiText.fontSize = (int)(12 * Screen.width / 800f);
+			guiText.alignment = TextAlignment.Center;
+			guiText.anchor = TextAnchor.MiddleCenter;
+			guiText.pixelOffset = new Vector2(choiceBounds[i].center.x, choiceBounds[i].y - guiText.fontSize);
+			guiText.color = Color.black;
+
+		}
 	}
 
 	public override void TriggerFinish ()
 	{
-
-
+		Destroy (dialogueWindow);
 	}
 }
