b6ac9717f5d6992173ef9fc2270d60d588522eef
Scarmach <scarmach@gmail.com>
2014-05-15 15:28:35 +0200

    clean dat shit

--- Assets/Scripts/LevelLoadTest.cs
+++ Assets/Scripts/LevelLoadTest.cs
@@ -5,6 +5,8 @@ public class LevelLoadTest : MonoBehaviour {
 	public string levelToLoad;
 
 	void OnMouseDown(){
+        if (this.gameObject.name == "NyttSpel")
+            PlayerPrefs.DeleteAll();
 		Application.LoadLevel(levelToLoad);
 		Debug.Log ("PRESSED");
 	}
