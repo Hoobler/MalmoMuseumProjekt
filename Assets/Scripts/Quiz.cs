0d733704254ed760bd72b75f30d1bb810bffe875
Bernhard Hedlund <orvarsmejl@hotmail.com>
2014-05-15 11:20:19 +0200

    quizfix

--- Assets/Scripts/Quiz.cs
+++ Assets/Scripts/Quiz.cs
@@ -1,5 +1,7 @@
 using UnityEngine;
 using System.Collections;
+using System.Text;
+using System.Text.RegularExpressions;
 
 [System.Serializable]
 public class Question{
@@ -25,7 +27,7 @@ public class Quiz : MonoBehaviour {
 	int points;
 	bool isChoosingAnswer;
 
-
+	bool previousActionWasMouseButtonDown = false;
 
 	int selectedAnswer;
 
@@ -62,43 +64,61 @@ public class Quiz : MonoBehaviour {
 
 	void Update() {
 		if (quizActive) {
-			if(Input.GetMouseButtonDown(0))
+			if(Input.GetMouseButtonDown(0) && !previousActionWasMouseButtonDown)
 			{
-				if(isChoosingAnswer)
-				{
-					if (listOfQuestions.Count != 0)
+				previousActionWasMouseButtonDown = true;
+				if(quizHasActuallyStarted){
+					if(isChoosingAnswer)
 					{
-						for(int i = 0; i < questions [(int)listOfQuestions[currentQuestion]].answers.Length; i++)
+						if (listOfQuestions.Count != 0)
 						{
-							if(((GUITexture)buttons[i].GetComponentInChildren(typeof(GUITexture))).GetScreenRect().Contains(Input.mousePosition))
+							for(int i = 0; i < questions [(int)listOfQuestions[currentQuestion]].answers.Length; i++)
 							{
-								selectedAnswer = i;
-								if(selectedAnswer == questions [(int)listOfQuestions[currentQuestion]].correctAnswer)
+								if(buttons[i].guiTexture.GetScreenRect().Contains(Input.mousePosition))
 								{
-									points++;
-									Instantiate(Resources.Load ("FadeCorrect"));
-								}
-								else
-									Instantiate(Resources.Load ("FadeWrong"));
-								isChoosingAnswer = false;
+									selectedAnswer = i;
+									if(selectedAnswer == questions [(int)listOfQuestions[currentQuestion]].correctAnswer)
+									{
+										points++;
+										Instantiate(Resources.Load ("FadeCorrect"));
+									}
+									else
+										Instantiate(Resources.Load ("FadeWrong"));
+									isChoosingAnswer = false;
 
-								((GUITexture)buttons[selectedAnswer].GetComponentInChildren(typeof(GUITexture))).color = Color.red;
-								((GUITexture)buttons[questions [(int)listOfQuestions[currentQuestion]].correctAnswer].GetComponentInChildren(typeof(GUITexture))).color = Color.green;
-								break;
+									buttons[selectedAnswer].guiTexture.color = Color.red;
+									buttons[questions [(int)listOfQuestions[currentQuestion]].correctAnswer].guiTexture.color = Color.green;
+									break;
+								}
 							}
+						}
+						else
+						{
+							TriggerFinish();
 						}
 					}
 					else
 					{
-						TriggerFinish();
+						isChoosingAnswer = true;
+						NextQuestion();
 					}
 				}
 				else
 				{
-					isChoosingAnswer = true;
-					NextQuestion();
+					if(buttons[0].guiTexture.GetScreenRect().Contains(Input.mousePosition))
+					{
+						quizHasActuallyStarted = true;
+						Destroy(quizParent);
+						ActivateQuiz();
+					}
+					else if(buttons[1].guiTexture.GetScreenRect().Contains(Input.mousePosition))
+					{
+						TriggerFinish();
+					}
 				}
 			}
+			else
+				previousActionWasMouseButtonDown = false;
 		}
 	}
 
@@ -117,17 +137,44 @@ public class Quiz : MonoBehaviour {
 		}
 	}
 
-	public void ActivatePreQuiz()
+	void ActivatePreQuiz()
 	{
-		GameObject background = new GameObject ("Quizpredialogue");
+		quizParent = new GameObject ("QuizParent");
+		GameObject background = new GameObject ("QuizPredialogue");
+		background.transform.parent = quizParent.transform;
+		background.AddComponent<GUIText> ();
+		background.guiText.fontSize = (int)(14 * Screen.width / 800f);
+        background.guiText.text = "Så du vill svara på några frågor om Lilla Torg?";
+		//hbackground.guiText.text = Encoding.Unicode.GetString(Encoding.UTF8.GetBytes(questionText));
+
+
 		background.AddComponent<GUITexture> ();
 		background.guiTexture.texture = texture;
-		background.guiTexture.pixelInset = new Rect (0, 0, 0, 0);
+		background.transform.position = new Vector3(0,0,-1);
+		background.transform.localScale = Vector3.zero;
+		background.guiTexture.pixelInset = new Rect (Screen.width * 0.3f, Screen.height *0.3f, Screen.width * 0.4f, Screen.height * 0.4f);
+		background.guiText.pixelOffset = new Vector2 (background.guiTexture.GetScreenRect ().x + background.guiTexture.GetScreenRect ().width * 0.2f, background.guiTexture.GetScreenRect ().yMax - background.guiTexture.GetScreenRect ().height * 0.2f);
+		background.guiText.color = Color.black;
+
+		buttons = new GameObject[2];
+		buttons[0] = (GameObject)Instantiate (Resources.Load ("Button"));
+		buttons [0].transform.parent = quizParent.transform;
+		buttons[0].guiText.text = "JA";
+		buttons[0].guiText.fontSize = (int)(12 * Screen.width / 800f);
+		buttons[0].guiText.pixelOffset = new Vector2 (Screen.width * 0.4f, Screen.height * 0.45f);
+		buttons[0].guiTexture.pixelInset = new Rect (Screen.width * 0.35f, Screen.height * 0.4f, Screen.width * 0.1f, Screen.height * 0.1f);
+
+		buttons[1] = (GameObject)Instantiate (Resources.Load ("Button"));
+		buttons [1].transform.parent = quizParent.transform;
+		buttons[1].guiText.text = "NEJ";
+		buttons[1].guiText.fontSize = (int)(12 * Screen.width / 800f);
+		buttons[1].guiText.pixelOffset = new Vector2 (Screen.width * 0.6f, Screen.height * 0.45f);
+		buttons[1].guiTexture.pixelInset = new Rect (Screen.width * 0.55f, Screen.height * 0.4f, Screen.width * 0.1f, Screen.height * 0.1f);
 	}
 
-	public void ActivateQuiz()
+	void ActivateQuiz()
 	{
-
+		quizParent = new GameObject ("QuizParent");
 		GameObject background = (GameObject)Instantiate (Resources.Load ("Quizmain"));
 		background.transform.parent = quizParent.transform;
 		
@@ -154,11 +201,20 @@ public class Quiz : MonoBehaviour {
 		for (int i = 0; i < 4; i++) {
 			buttons [i] = (GameObject)Instantiate (Resources.Load ("Button"));
 			buttons[i].transform.parent = quizParent.transform;
-			((GUIText)buttons[i].GetComponentInChildren (typeof(GUIText))).fontSize = (int)(12 * Screen.width / 800f);
+			buttons[i].transform.position = new Vector3 (0,0,1);
+			buttons[i].guiText.fontSize = (int)(12 * Screen.width / 800f);
 			if(i%2 == 0)
-				buttons[i].transform.position = new Vector3(0.5f - 0.15f, 0.25f+0.1f*(i/2), 0); 
-			if(i%2 == 1)
-				buttons[i].transform.position = new Vector3(0.5f + 0.15f, 0.25f+0.1f*(i/2), 0);
+			{
+				//buttons[i].guiText.pixelOffset = new Vector2(Screen.width * (0.5f - 0.15f), Screen.height * (0.25f+0.1f*(i/2)));
+				buttons[i].guiText.pixelOffset = new Vector2(backgroundBounds.x + backgroundBounds.width/4f, backgroundBounds.yMax -  backgroundBounds.height * 0.5f - (i/2) * backgroundBounds.height * 0.25f);
+				//buttons[i].guiTexture.pixelInset = new Rect(Screen.width * (0.5f - 0.30f), Screen.height * (0.25f+0.1f*(i/2)), Screen.width * 0.3f, Screen.height*0.2f);
+			}
+			else if(i%2 == 1)
+			{
+				buttons[i].guiText.pixelOffset = new Vector2(backgroundBounds.x + backgroundBounds.width* 3f/4f, backgroundBounds.yMax - backgroundBounds.height * 0.5f - (i/2) * backgroundBounds.height * 0.25f);
+				//buttons[i].guiTexture.pixelInset = new Rect(Screen.width * (0.5f), Screen.height * (0.25f+0.1f*(i/2)), Screen.width * 0.3f, Screen.height*0.2f);
+			}
+			buttons[i].guiTexture.pixelInset = new Rect(buttons[i].guiText.GetScreenRect().center.x - Screen.width*0.08f, buttons[i].guiText.GetScreenRect().center.y - Screen.height*0.08f, Screen.width*0.16f, Screen.height*0.16f);
 		}
 		
 		SetValues ();
@@ -168,8 +224,8 @@ public class Quiz : MonoBehaviour {
 	{
 		quizHasActuallyStarted = false;
 
-		quizParent = new GameObject ("QuizParent");
-		ActivateQuiz ();
+
+		ActivatePreQuiz ();
 	}
 
 	public void TriggerFinish()
@@ -197,7 +253,7 @@ public class Quiz : MonoBehaviour {
 		for (int i = 0; i < questions [(int)listOfQuestions[currentQuestion]].answers.Length; i++) { 
 			SetButtonText(buttons [i], questions [(int)listOfQuestions[currentQuestion]].answers [i]);
 			SetButtonEnabled(buttons[i], true);
-			((GUITexture)buttons[i].GetComponentInChildren(typeof(GUITexture))).color = Color.black;
+			buttons[i].guiTexture.color = Color.black;
 		}
 		for (int i = questions [(int)listOfQuestions[currentQuestion]].answers.Length; i < 4; i++)
 			SetButtonEnabled(buttons[i], false);
@@ -205,22 +261,20 @@ public class Quiz : MonoBehaviour {
 
 	void SetButtonEnabled(GameObject button, bool desiredState)
 	{
-		GUIText text = (GUIText)button.GetComponentInChildren (typeof(GUIText));
-		GUITexture background = (GUITexture)button.GetComponentInChildren (typeof(GUITexture));
-
-		text.enabled = desiredState;
-		background.enabled = desiredState;
+		button.guiText.enabled = desiredState;
+		button.guiTexture.enabled = desiredState;
 	}
 
 	void SetButtonText(GameObject button, string text)
 	{
-		((GUIText)button.GetComponentInChildren (typeof(GUIText))).text = text;
+		button.guiText.text = text;
 	}
 
 	void OnMouseDown()
 	{
 		if (!quizActive && !GameObject.Find ("Quest_Handler").GetComponent<QuestManager> ().IsQuestInProgress()) {
 			TriggerStart ();
+			previousActionWasMouseButtonDown = true;
 			quizActive = true;
 		}
 	}
