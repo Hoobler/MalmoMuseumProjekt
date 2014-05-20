0daebf312e9c7d3cee334f3ec5795caaf2000529
Joakim Persson <sir.hoobler@gmail.com>
2014-05-20 15:25:20 +0200

    Derp

--- Assets/Scripts/QuestScripts/ShowOnQuestStart.cs
+++ Assets/Scripts/QuestScripts/ShowOnQuestStart.cs
@@ -10,24 +10,24 @@ public class ShowOnQuestStart : MonoBehaviour {
 	}
 	
 	void ShowOnQuest(object o, QuestEventArgs e){
-		Debug.Log("ShowOnQuest");
-		if(e.MiniGames == MiniGamesEnum.Musköt){
-			Debug.Log("miniEnum: " + e.MiniGames);
-			Debug.Log("ShowOnQuest inside miniEnum");
-			if(e.QuestType == QuestTypeEnum.Started){
-				Debug.Log("ShowOnQuest evArgs QuestStarted");
-				if(this.renderer != null){
-					Debug.Log("Showonquest not null");
-				}
-				if(this.renderer == null){
-					Debug.Log("Showonquest is null");
-				}
-				this.renderer.enabled = true;
-			}
-			if(e.QuestType == QuestTypeEnum.Finnished){
-				Debug.Log("ShowOnQuest evArgs QuestFin");
-				this.renderer.enabled = false;
-			}
-		}
+//		Debug.Log("ShowOnQuest");
+//		if(e.MiniGames == MiniGamesEnum.Musköt){
+//			Debug.Log("miniEnum: " + e.MiniGames);
+//			Debug.Log("ShowOnQuest inside miniEnum");
+//			if(e.QuestType == QuestTypeEnum.Started){
+//				Debug.Log("ShowOnQuest evArgs QuestStarted");
+//				if(this.renderer != null){
+//					Debug.Log("Showonquest not null");
+//				}
+//				if(this.renderer == null){
+//					Debug.Log("Showonquest is null");
+//				}
+//				this.renderer.enabled = true;
+//			}
+//			if(e.QuestType == QuestTypeEnum.Finnished){
+//				Debug.Log("ShowOnQuest evArgs QuestFin");
+//				this.renderer.enabled = false;
+//			}
+//		}
 	}
 }
