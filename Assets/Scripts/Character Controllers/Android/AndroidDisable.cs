668a17dc10b2c2c1f98c582e7b40de5dcc56de84
Joakim Persson <sir.hoobler@gmail.com>
2014-05-28 15:53:14 +0200

    Herp
    Funkar inte just nu ska fixa!

--- Assets/Scripts/Character Controllers/Android/AndroidDisable.cs
+++ Assets/Scripts/Character Controllers/Android/AndroidDisable.cs
@@ -10,10 +10,10 @@ public class AndroidDisable: MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		EventManager.DisableAndroidEvent += new DisableAndroid(DisableThumbPads);
-//		LeftPad = GameObject.Find("LeftTouchPad");
-//		RightPad = GameObject.Find("RightTouchPad");
-//		TouchPads = new GameObject[2];
-//		TouchPads = GameObject.FindGameObjectsWithTag("TouchPad");
+		LeftPad = GameObject.Find("LeftTouchPad");
+		RightPad = GameObject.Find("RightTouchPad");
+		TouchPads = new GameObject[2];
+		TouchPads = GameObject.FindGameObjectsWithTag("TouchPad");
 	}
 
 	void OnEnable(){
@@ -25,19 +25,24 @@ public class AndroidDisable: MonoBehaviour {
 	void DisableThumbPads(object o ,AndroidDisableArgs e){
 		Debug.Log("Disable test");
 		Debug.Log("Args: " + e.Disable);
-		if(TouchPads[0]){
-			Debug.Log("is not null");
-		}else{
-			Debug.Log("Leftpad is null");
-		}
+//		if(TouchPads[0]){
+//			Debug.Log("is not null activeSelf: " + TouchPads[0].activeSelf);
+//		}else{
+//			Debug.Log("Leftpad is null");
+//		}
 		if(e.Disable){
-			LeftPad.SetActive(false);
-			RightPad.SetActive(false);
+//			LeftPad.SetActive(false);
+//			RightPad.SetActive(false);
+			LeftPad.GetComponent(GUITexture).renderer.enabled = false;
+			RightPad.GetComponent(GUITexture).renderer.enabled = false;
+			Debug.Log("Disable activeSelf: " + TouchPads[0].activeSelf);
 		}
 		if(!e.Disable){
-			Debug.Log("SetActive True");
-			LeftPad.SetActive(true);
-			RightPad.SetActive(true);
+//			LeftPad.SetActive(true);
+//			RightPad.SetActive(true);
+			LeftPad.renderer.enabled = true;
+			RightPad.renderer.enabled = true;
+			Debug.Log("Enable activeSelf: " + TouchPads[0].activeSelf);
 		}
 	}
 }
